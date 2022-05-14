565e4be11b18c876717b8d49f69dd3fef0606c5f
Alex <abadesgrimes31@gmail.com>
2022-05-14 15:36:42 +0200

    Union find, trees

--- Deep Python/Union Find/Union Find.py
+++ Deep Python/Union Find/Union Find.py
@@ -101,3 +101,57 @@ a.disp()
 print(a.find(2))
 a.union(3,2)
 a.disp()
+
+
+#%% 
+
+class QuickFind():
+    """
+    Mantains a tree structure in the unions, makes the union on the parent tree
+    """
+    
+    
+    
+    def __init__(self, n):
+        """
+        Time Complexity O(n)
+        Space complexity O(n)
+        """
+        self.n = n 
+        self.p = [i for i in range(n)]
+        self.size = [i for i in range(n)]
+    
+    def find(self,i):
+        """
+        The only one that has the same index is the root.
+        Time Complexity O(d)
+            Where d is the depth of the tree
+        """
+        while i != self.p[i]:
+            i = self.p[i]
+        return i
+    
+    def union(self, i,j):
+        """
+        Time complexity O(d)
+            d-> Depth of the tree 
+        Problem: The depth can be n, as it can happen that the 
+        """
+        
+        root_i = self.find(i)
+        root_j = self.find(j)
+        
+        # Check that the root it's not the same in both trees, se we can join them
+        if root_i != root_j:
+            # Check The dimensions of the tree 
+            if self.size[root_i] < self.size[root_j]:
+                # If th brach i is smaller than the branh j, we join i to j
+                self.p[root_i] = root_j
+                self. size[root_j] += self.size[root_i]
+            else:
+                self.p[root_j] = self.p[root_i]
+                self.size[root_i] += self.size[root_j]
+        
+    
+    def disp(self):
+        print(self.p)
